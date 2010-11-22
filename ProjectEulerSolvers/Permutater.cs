ccdb357f66d33b83491138407760d27ff3aa1e43
damnstout <damnstout@gmail.com>
2010-11-23 02:32:19 +0800

    working on problem 062

---
+++ ProjectEulerSolvers/Permutater.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ProjectEulerSolvers
+{
+    class Permutater<T, K> : IEnumerable<T>, IEnumerator<T> where T : IEnumerable<K>
+    {
+        private T a;
+
+
+        #region IDisposable 成员
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IEnumerator 成员
+
+        public T Current
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        object System.Collections.IEnumerator.Current
+        {
+            get { throw new NotImplementedException(); }
+        }
+
+        public bool MoveNext()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Reset()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+
+        #region IEnumerable<T> 成员
+
+        public IEnumerator<T> GetEnumerator()
+        {
+            return this;
+        }
+
+        #endregion
+
+        #region IEnumerable 成员
+
+        System.Collections.IEnumerator System.Collections.IEnumerable.GetEnumerator()
+        {
+            throw new NotImplementedException();
+        }
+
+        #endregion
+    }
+}
